4f08f0918053674b32fa5b0a03744bdeb077c6f7
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-10-19 13:49:45 -0500

    server test for Micheal

--- src/pages/Meeting.tsx
+++ src/pages/Meeting.tsx
@@ -108,10 +108,8 @@ const Meeting = (): JSX.Element => {
 
   const { user } = useAuth();
   const { connected, emit, on, off } = useSocket(
-    import.meta.env.VITE_SOCKET_URL || "http://localhost:3000/",
-    // wss://134.199.193.207:3001/
-
-
+    import.meta.env.VITE_SOCKET_URL || "wss://134.199.193.207:3001/",
+    // wss://134.199.193.207:3001
     { withCredentials: false }
   );
 
